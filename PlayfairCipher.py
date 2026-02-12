8d864168ba6d551ccd7fcb2f698790a1a558d2b5
Juan Luis Arteaga <juanluis.arteaga.mantovani1@gmail.com>
2026-02-12 18:39:26 -0500

    Created base strucutre of code

---
+++ PlayfairCipher.py
@@ -0,0 +1,19 @@
+def encrypt(key, message):
+  key = ""
+  encryptedMessage = ""
+  table = [[],
+          [],
+          [],
+          [],
+          []]
+
+  return encryptedMessage
+
+def generateMatrix(key,size):
+
+
+def decrypt(key, encryptedMessage):
+  decryptedMessage = ""
+  return decryptedMessage
+
+
