14656693757717036de654022ae5a3bdc3834d90
Juan Luis Arteaga <juanluis.arteaga.mantovani1@gmail.com>
2026-02-13 19:05:26 -0500

    Insert key into matrix -badly- implemented

--- PlayfairCipher.py
+++ PlayfairCipher.py
@@ -1,3 +1,5 @@
+import string
+
 def encrypt(key, message):
   key = ""
   encryptedMessage = ""
@@ -6,7 +8,41 @@ def encrypt(key, message):
   return encryptedMessage
 
 def generateMatrix(key):
-  keyMatrix = 5*[]
+  keyMatrix = [
+              [],
+              [],
+              [],
+              [],
+              []
+              ]
+  key = list(key.lower())
+  characters = list(string.ascii_lowercase)
+  temp1 = len(key)
+
+  temp = 0
+
+  for i in range(temp1):
+    try:
+      characters.remove(key[i])
+    except ValueError:
+      key[i] = 0
+      temp += 1
+
+  for i in range(temp):
+    key.remove(0)
+
+  keySize = len(key)
+  print(keySize)
+  temp = 5
+  temp1 = 0
+
+  for i in range(keySize//5 + keySize%5 + 1):
+    keyMatrix[i] = list(key[temp1:temp])
+    temp1 += 5
+    temp += 5
+  for i in range(keySize//5 + keySize%5, 5):
+    keyMatrix[i] = list(characters[0:5])
+
 
   return keyMatrix
 
@@ -15,4 +51,4 @@ def decrypt(key, encryptedMessage):
   return decryptedMessage
 
 ## test bench
-print(generateMatrix("yoanpinzon"))
+print(generateMatrix("YoanPinzon"))
