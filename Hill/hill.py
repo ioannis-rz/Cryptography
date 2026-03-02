d302e02fa479335957d4035674988ef8b71a5a07
Juan Luis Arteaga <juanluis.arteaga.mantovani1@gmail.com>
2026-03-01 20:33:08 -0500

    Created some matrix verification functions

--- Hill/hill.py
+++ Hill/hill.py
@@ -1,9 +1,26 @@
+import numpy as np
+
 def hill(key, cyphertext):
     cleartext=""
     return cleartext
 
-
-
 def hill(key, cleartext):
     cyphertext = ""
     return cyphertext
+
+def det(matrix: np.array):
+    det = np.linalg.det(matrix)
+    return det
+
+def hasInverse(matrix: np.array):
+    if (matrix.shape != (2,2)):
+        raise Exception("Matrix size must be 2x2")
+    if (det(matrix) != 0):
+        return True
+    else: return False
+
+test2 = np.array([[2,1],[1,1]])
+test = np.zeros((2,2), dtype=int)
+print(det(test2))
+
+print(hasInverse(test2))
