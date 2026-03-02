2fe777684216cc4ba8a03321b17d2a3e2f18c4e5
Juan Luis Arteaga <juanluis.arteaga.mantovani1@gmail.com>
2026-03-02 15:10:52 -0500

    Cleaned up majority of functoins and added stronger validation for matrices

--- Hill/hill.py
+++ Hill/hill.py
@@ -5,10 +5,12 @@ ALPHABET_SIZE = 26
 
 def decryptHill(key: np.array, ciphertext: str):
     cleartext=""
-    ciphertext = re.sub("[^A-Za-z]", "", ciphertext).lower()
+    ciphertext = cleanText(ciphertext)
     if len(ciphertext) % 2 != 0:
-        ciphertext += 'x'
+        raise ValueError("Longitud del ciphertext debe ser par")
+
     keyInv = inverseModular(key, ALPHABET_SIZE)
+
     for i in range(0, len(ciphertext), 2):
         temp = np.array([ord(ciphertext[i]) - ord('a'), ord(ciphertext[i+1]) - ord('a')])
         temp = np.matmul(temp,keyInv) % ALPHABET_SIZE
@@ -17,34 +19,37 @@ def decryptHill(key: np.array, ciphertext: str):
 
 def encryptHill(key: np.array, cleartext: str):
     ciphertext = ""
-    cleartext = re.sub("[^A-Za-z]", "", cleartext).lower()
+    cleartext = cleanText(cleartext)
     if len(cleartext) % 2 != 0:
         cleartext += 'x'
+
+    if key.shape != (2,2):
+        raise ValueError("Matriz debe ser 2x2")
+    if (eea(det(key),ALPHABET_SIZE)[0]) != 1:
+        raise ValueError("Matriz no tiene inversa modular")
+
     for i in range(0, len(cleartext), 2):
         temp = np.array([ord(cleartext[i]) - ord('a'), ord(cleartext[i+1]) - ord('a')])
         temp = np.matmul(temp,key) % ALPHABET_SIZE
         ciphertext += chr(temp[0] + ord('a')) + chr(temp[1] + ord('a'))
     return ciphertext
 
+
+
 def det(matrix: np.array):
     if (matrix.shape != (2,2)):
-        raise Exception("Matriz debe tener tamaño 2x2")
+        raise ValueError("Matriz debe tener tamaño 2x2")
     return matrix[0][0]*matrix[1][1] - matrix[0][1]*matrix[1][0]
 
 def adj(matrix: np.array):
     if (matrix.shape != (2,2)):
-        raise Exception("Matriz debe tener tamaño 2x2")
+        raise ValueError("Matriz debe tener tamaño 2x2")
     adjMatrix = matrix.copy()
     adjMatrix[0][0] = matrix[1][1]
     adjMatrix[1][1] = matrix[0][0]
     adjMatrix[0][1] = -matrix[0][1]
     adjMatrix[1][0] = -matrix[1][0]
     return adjMatrix
-
-def hasInverse(matrix: np.array):
-    if (det(matrix) != 0):
-        return True
-    else: return False
 
 def eea(a: int, b: int):
     if b == 0:
@@ -54,22 +59,23 @@ def eea(a: int, b: int):
     (d, x, y) = (d, y, x - q * y)
     return (d, x, y)
 
+def cleanText(text: str):
+    textCorrected = re.sub("[^A-Za-z]", "", text).lower()
+    return textCorrected
+
 def inverseModular(matrix: np.array, mod: int):
-    if not hasInverse(matrix):
-        raise Exception("La matriz no tiene inversa")
-    mat = adj(matrix)
+    if matrix.shape != (2,2):
+        raise ValueError("Matriz debe ser 2x2")
     eeaResult = eea(det(matrix), mod)
     if eeaResult[0] != 1:
-        raise Exception("La matriz no tiene inversa modular")
-    
+        raise ValueError("Matriz no tiene inversa modular")
+    mat = adj(matrix)
     return (mat * eeaResult[1]) % mod
 
-test2 = np.array([[3,7],[5,12]])
-text = "Herbert Yardley wrote The American Black Chamber"
+
+test2 = np.array([[11,8],[3,7]])
+text = "JULY"
 cipher = encryptHill(test2, text)
-#print(adjunta(test2))
-#print(det(test2))
-# print(inverseModular(test2, 26))
 cleartext = decryptHill(test2, cipher)
 print("Ciphertext: " + cipher)
 print("Cleartext: " + cleartext)
