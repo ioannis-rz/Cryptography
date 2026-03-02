5cebd4205cf91d6fb4812f9855b85b148aeb928d
Juan Arteaga <juanluis.arteaga.mantovani1@gmail.com>
2026-03-02 10:50:02 -0500

    Implement decryption functionality and enhance matrix operations with error handling

--- Hill/hill.py
+++ Hill/hill.py
@@ -3,6 +3,11 @@ import re
 
 def decryptHill(key: np.array, ciphertext: str):
     cleartext=""
+    keyInv = inverseModular(key, 26)
+    for i in range(0, len(ciphertext), 2):
+        temp = np.array([ord(ciphertext[i]) - ord('a'), ord(ciphertext[i+1]) - ord('a')])
+        temp = np.matmul(temp,keyInv) % 26
+        cleartext += chr(temp[0] + ord('a')) + chr(temp[1] + ord('a'))
     return cleartext
 
 def encryptHill(key: np.array, cleartext: str):
@@ -16,14 +21,48 @@ def encryptHill(key: np.array, cleartext: str):
         ciphertext += chr(temp[0] + ord('a')) + chr(temp[1] + ord('a'))
     return ciphertext
 
-def hasInverse(matrix: np.array):
+def det(matrix: np.array):
     if (matrix.shape != (2,2)):
-        raise Exception("Matrix size must be 2x2")
-    if (int(np.linalg.det(matrix)) != 0):
+        raise Exception("Matriz debe tener tamaño 2x2")
+    return matrix[0][0]*matrix[1][1] - matrix[0][1]*matrix[1][0]
+
+def adjunta(matrix: np.array):
+    if (matrix.shape != (2,2)):
+        raise Exception("Matriz debe tener tamaño 2x2")
+    temp = matrix[0][0]
+    matrix[0][0] = matrix[1][1]
+    matrix[1][1] = temp
+    matrix[0][1] = -matrix[0][1]
+    matrix[1][0] = -matrix[1][0]
+    return matrix
+
+def hasInverse(matrix: np.array):
+    if (det(matrix) != 0):
         return True
     else: return False
+
+def eea(a: int, b: int):
+    if b == 0:
+        return (a, 1, 0)
+    (d, x, y) = eea(b, a % b)
+    q = a // b
+    (d, x, y) = (d, y, x - q * y)
+    return (d, x, y)
+
+def inverseModular(matrix: np.array, mod: int):
+    if not hasInverse(matrix):
+        raise Exception("La matriz no tiene inversa")
+    
+    mat = adjunta(matrix)
+    print("Adjunta:\n" + str(mat))
+    return (mat * eea(det(matrix), mod)[1]) % mod
 
 test2 = np.array([[11,8],[3,7]])
 text = "JULY"
 cipher = encryptHill(test2, text)
-print(cipher)
+#print(adjunta(test2))
+#print(det(test2))
+# print(inverseModular(test2, 26))
+cleartext = decryptHill(test2, cipher)
+print("Ciphertext: " + cipher)
+print("Cleartext: " + cleartext)
