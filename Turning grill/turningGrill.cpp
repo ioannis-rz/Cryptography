815de8204797da54dd87fe8c5d94f59d393c95cd
Juan Arteaga <juanluis.arteaga.mantovani1@gmail.com>
2026-03-06 16:52:22 -0500

    implemented basic version of encryption

--- Turning grill/turningGrill.cpp
+++ Turning grill/turningGrill.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string> 
 #include <vector>
+#include <algorithm>
 using namespace std;
 
 constexpr bool LEFT = true;
@@ -22,9 +23,29 @@ void printArr(T* ptr, int size) {
     }
     cout << endl; 
 }
+template <typename T>
+void printVector(const vector<T>& vector1) {
+    for (T item : vector1) {
+        cout << item << " ";
+    }
+    cout << endl;
+}
 
-vector<int> rotate(vector<int>& holes, bool rotDirection){
-    
+vector<int> rotate(vector<int>& holes, bool rotDirection, int sizeMatrix){
+    // printVector(holes);
+    for (int& num : holes){
+        int row = num / sizeMatrix; // size es dimension n de la matrix
+        int col = num % sizeMatrix;
+        int newNum;
+        if (rotDirection == RIGHT) {
+            newNum = col * sizeMatrix + (sizeMatrix-1-row); // funciona
+        } else {
+            newNum = (sizeMatrix-1-col)*sizeMatrix + row; 
+        }
+        num = newNum;
+    }
+    sort(holes.begin(),holes.end());
+    //printVector(holes);
     return holes;
 }
 
@@ -33,23 +54,28 @@ string encrypt(string cleartext, bool rotDirection, int size, vector<int> holes)
     ciphertext[size*size] = '\0';
     
     for (int i = 0; i < size*size; i++) {
-        ciphertext[i] = '^';
+        ciphertext[i] = 'x';
     }
     
-    //ciphertext.reserve(16); // para ejemplo, reserva tamano 16
-    //int* arr = new int[size * size];
     int i = 0;
-    while (i < 4) {
+    int j = 0;
+    while (i < cleartext.size()) { // realizar para todas las letras del texto claro
+        if ((i % holes.size() == 0) && i!=0){ // rotar cadd 4 iteraciones
+            holes = rotate(holes, rotDirection, size); // rotar la matriz = cambiar los huecos
+            cout << "Executed rotation, nuevos huecos en ";
+            printVector(holes);
+            //cout << i << endl;
+            j = 0; // resetear posicion inicial para holes
+        }
         // colocar en posicion del texto cifrado (holes) el caracter del texto claro
-        ciphertext[holes[i]] = cleartext.at(i); // quiza sea necesario hacer una verificacion apra esto
-        rotate(holes, rotDirection); // rotar la matriz = cambiar los huecos
+        ciphertext[holes[j]] = cleartext.at(i); // quiza sea necesario hacer una verificacion apra esto
+        printArr(ciphertext, size*size+1);
         i++;
+        j++;
     }
-    
+
     // printArr(ciphertext, size*size);
-    
     string s = ciphertext;
-    cout << s << endl;
     return s;
 }
 
@@ -62,7 +88,7 @@ string decrypt(string ciphertext, bool rotDirection, int size, vector<int> holes
 int main() {
     cout << "Hello World" << endl;
     vector<int> huecos = {0, 9, 11, 14}; // si se empiezan a contar los huecos desde la posicion 0
-    string texto = encrypt("jimattacksatdawn", LEFT, 4, huecos);
+    string texto = encrypt("jimattackatdawn", LEFT, 4, huecos);
     cout << "El texto encriptado es: " + texto << endl;
     return 0;
 }
