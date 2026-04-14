8af798cbeabb0a4450281d42a2547e2cda6cf099
Juan Luis Arteaga <juanluis.arteaga.mantovani1@gmail.com>
2026-04-14 11:59:37 -0500

    Removed previous code

--- DES/DES.py
+++ DES/DES.py
@@ -28,8 +28,10 @@ if __name__ == "__main__":
     with open('PICT4219.JPG', 'rb') as file:
         image = file.read()
     # encrypt the image
+    print("Encrypting")
     ciphertext = des_encrypt_data(key, iv, image)
     # encode in base64
+    print("Encoding in Base64")
     ciphertext_b64 = base64.standard_b64encode(ciphertext)
 
     print("Mensaje cifrade en Base64")
@@ -37,18 +39,13 @@ if __name__ == "__main__":
 
     # decryption
     # decode
+    print("Decoding")
     ciphertext2 = base64.standard_b64decode(ciphertext_b64)
     # decrypt
+    print("Decrypting")
     cleartext2 = des_decrypt_data(key, iv, ciphertext2)
     # save to image
     with open('PICT4219(1).JPG', 'wb') as file:
         file.write(cleartext2)
+
     display_image(cv2.imread('PICT4219(1).JPG'))
-
-    cleartext = "Texto a ser encriptado"
-    des = pyDes.des(key, mode=pyDes.CBC, IV=iv, pad=None, padmode=pyDes.PAD_PKCS5)
-    ciphertext = des.encrypt(cleartext, pad=None, padmode=pyDes.PAD_PKCS5)
-    decrypted = des.decrypt(ciphertext, pad=None, padmode=pyDes.PAD_PKCS5)
-    print("Ciphertext is: ", ciphertext)
-    print("The decrypted text is: ", decrypted)
-
